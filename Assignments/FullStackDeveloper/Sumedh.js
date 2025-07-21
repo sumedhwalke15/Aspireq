5988b906f7140ffc22b5a73b264cae6278d87985
sumedhwalke <sumedhwalke15@gmail.com>
2025-07-21 15:46:27 +0530

    Re-added Assignments as a normal folder

---
+++ Assignments/FullStackDeveloper/Sumedh.js
@@ -0,0 +1,12 @@
+ let a=30
+let x=++a
+console.log("x ",x)
+console.log("a ",a)
+
+let y=a++
+console.log("y ",y)
+console.log("a ",a)
+
+let z=--a
+console.log("z ",z)
+console.log("a ",a)
---
+++ Assignments/Logical Reasoning/addition.java
@@ -0,0 +1,9 @@
+public class addition {
+    public static void main(String[] args) {
+        int a=5;
+        int b=10;
+        int sum = a +b;
+        System.out.println("a+b="+sum);
+
+    }
+}
